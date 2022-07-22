aded3e6fa9780ec482f8485cc0ec88e129c3bb15
Koray Ademoglu <korayademoglu@gmail.com>
2022-07-22 22:57:48 +0300

    Create Element OL

--- custom.js
+++ custom.js
@@ -1,8 +1,8 @@
 
 let data = fetch("data.json").then(res => res.json()).then(mydata => {
-
+ document.createElement("OL");
     for (let i = 0; i < mydata.kullanicilar.length; i++) {
-        document.write((i + 1) + ". " + mydata.kullanicilar[i].name + " " + mydata.kullanicilar[i].surname + " Yaşı:" + mydata.kullanicilar[i].age + "<br>");
+        document.getElementById("test").innerHTML += "<li>" + (i + 1) + ". " + mydata.kullanicilar[i].name + " " + mydata.kullanicilar[i].surname + " Yaşı:" + mydata.kullanicilar[i].age + "</li>";
     }
 
 });
