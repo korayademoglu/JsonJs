1294d6acad4c3001b97008c2de387cfbbc1c9022
Koray Ademoglu <korayademoglu@gmail.com>
2022-07-22 23:53:02 +0300

    Updated list

--- custom.js
+++ custom.js
@@ -1,8 +1,15 @@
 
 let data = fetch("data.json").then(res => res.json()).then(mydata => {
- document.createElement("OL");
+    let ol = document.createElement("OL");
+    ol.setAttribute("id", "myOl");
+    document.getElementById("test").appendChild(ol);
     for (let i = 0; i < mydata.kullanicilar.length; i++) {
-        document.getElementById("test").innerHTML += "<li>" + (i + 1) + ". " + mydata.kullanicilar[i].name + " " + mydata.kullanicilar[i].surname + " Yaşı:" + mydata.kullanicilar[i].age + "</li>";
-    }
+        let li = document.createElement("LI");
+        let desc = document.createTextNode(mydata.kullanicilar[i].name + " "
+        + mydata.kullanicilar[i].surname + " Yaşı:" + mydata.kullanicilar[i].age);
+        li.appendChild(desc);
+        document.getElementById("myOl").appendChild(li);           
 
+    }
+    
 });
